b212e9ffa31d67430601efef92168a1e96c3a771
Brian Mote <busiensslikeaboss@gmail.com>
2020-08-04 14:28:52 -0700

    modified SearchBar component to take onSubmit function via props

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+
 import Logo from "./logo";
 import SearchBar from './searchBar';
 import RecentPosts from "./recentPosts";
 
 export default class Home extends Component {
+
+  handleSearchBarSubmit = function(query) {
+    console.log('trying to handle submit for query', query)
+    //navigate to a new route.
+    this.props.history.push('/results')
+}
+
   render() {
     return (
       <div>
         <div>
           <Logo />
-          <SearchBar />
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
            <RecentPosts />
         </div>
       </div>
--- src/components/results.js
+++ src/components/results.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Logo from './logo'
+import SearchBar from './searchBar'
 
 class Results extends Component {
+
+    handleSearchBarSubmit(query) {
+
+    }
     render() {
         return (
             <div>
-                <Logo size={55} />
-                <SearchBar />
+                <Logo size={55}/>
+                <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
             </div>
         )
     }
