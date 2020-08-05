277245c3afc113feda38f522fd0b5655d63f4bed
Brian Mote <busiensslikeaboss@gmail.com>
2020-08-05 09:52:46 -0700

    modified fetchPostsWithQuery actionCreator to take in an optional callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,7 +18,7 @@ export function fetchRecentPosts() {
     }
 }
 
-export function fetchPostsWithQuery(query) {
+export function fetchPostsWithQuery(query, callback) {
     return function(dispatch) {
         axios.get(`https://api.dailysmarty.com/search?q=${query}`)
             .then(response => {
@@ -27,6 +27,7 @@ export function fetchPostsWithQuery(query) {
                     type: SET_RESULTS_POSTS,
                     payload: response.data.posts
                 })
+                callback()
             })
     }
 }
